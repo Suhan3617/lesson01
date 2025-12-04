7f79ff04b16e801707a5aacaef7716958385a29c
Suhan3617 <suhanmakhare@gmail.com>
2025-12-04 15:35:20 +0530

    add user controller methods for CRUD operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -9,4 +9,23 @@ export class UsersController {
     PATCH /users/:id
     DELETE /users/:id
     */
+
+    @Get() // GET /users
+    findAll() {
+        return []
+    }
+
+    @Get('interns') // GET /users/interns
+    findInterns() {
+        return []
+    } 
+    @Get(':id') // GET /users/:id
+    findOne(@Param('id') id: string) {
+        return {id};
+    }
+
+    @Post()
+    create(@Body() user : {}) {
+        return user;
+    }
 }
