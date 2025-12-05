2d43c95f5eb52dd003c50c05907736cd385cdeee
Suhan3617 <suhanmakhare@gmail.com>
2025-12-05 12:00:09 +0530

    Adding Class Validator to the Create Dto and will be directly applied to the Update Dto

--- src/users/DTO/create-user.dto.ts
+++ src/users/DTO/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail , IsEnum , IsString , IsNotEmpty } from "class-validator";
+
 export class CreateDto{
+    @IsString()
+    @IsNotEmpty()
     name:string;
+
+    @IsEmail()
     email:string;
+
+    @IsEnum(["INTERN" , "EMPLOYEE"],{
+        message:'Valid Role Required'
+    })
     role:"INTERN" | "EMPLOYEE";
 }
