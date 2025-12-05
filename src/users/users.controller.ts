044f40e62ab5ad134351d58145f7b52adb7d6f15
Suhan3617 <suhanmakhare@gmail.com>
2025-12-05 11:10:04 +0530

    Adding of ParseIntPipe for transformation of string to number and also validating it

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   Query,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 
@@ -32,8 +33,8 @@ export class UsersController {
   //     return []
   // }
   @Get(':id') // GET /users/:id
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findOne(@Param('id' , ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
   }
 
   @Post() // POST /users
@@ -44,7 +45,7 @@ export class UsersController {
   }
   @Patch(':id')
   update(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body()
     userUpdate: {
       name?: string;
@@ -52,11 +53,11 @@ export class UsersController {
       role?: 'INTERN' | 'EMPLOYEE';
     },
   ) {
-    return this.usersService.update(+id, userUpdate);
+    return this.usersService.update(id, userUpdate);
   }
 
   @Delete(':id') // DELETE /users/:id
-  delete(@Param('id') id: string) {
-    return this.usersService.delete(+id);
+  delete(@Param('id' , ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
   }
 }
